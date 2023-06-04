350e9154f386ecb07997dbef8be3c09ab9449a73
shivani Kapatel <skapatel@my.centennialcollege.ca>
2023-06-04 13:08:22 -0400

    - Added html pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Hello World' });
+  res.render('home.ejs', { title: 'Hello World' });
 });
 
+/* GET aboutme page. */
+router.get('/aboutme', function (req, res, next) {
+  res.render('aboutme.ejs', { title: 'Hello World' });
+});
+
+/* GET projects page. */
+router.get('/projects', function (req, res, next) {
+  res.render('projects.ejs', { title: 'Hello World' });
+});
+
+/* GET projects page. */
+router.get('/contact', function (req, res, next) {
+  res.render('contact.ejs', { title: 'Hello World' });
+});
+
+/* GET services page. */
+router.get('/services', function (req, res, next) {
+  res.render('services.ejs', { title: 'Hello World' });
+});
 module.exports = router;
